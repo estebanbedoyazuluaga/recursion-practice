e642d135e9a6489c7e78e65cf6dd00b4666236d1
Esteban <ebedoya83134@umanizales.edu.co>
2021-03-18 17:30:49 -0500

    Added last method for assignment (number of possible paths "spiderman" can take)

--- recursion-practice/Program.cs
+++ recursion-practice/Program.cs
@@ -6,14 +6,12 @@ namespace recursion_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Sum of positive integers from 1 to 5 (expected "
-                              + $"value is 15): {SumIntegersFromOneToN(5)}");
-            Console.WriteLine(SumDigits(1999));
-            Console.WriteLine(IsPrime(113, 2));
+            Console.WriteLine($"Sum of positive integers from 1 to 5: {SumIntegersFromOneToN(5)}");
+            Console.WriteLine("Sum of the digits of the number 1999: " + SumDigits(1999));
+            Console.WriteLine($"Is 17 a prime number? {IsPrime(17)}");
+            Console.WriteLine($"If Spiderman has to swing through 5 buildings, " +
+                $"the number of ways (different paths) he can do it is: {CalculatePossiblePaths(5)}");
 
-            decimal a = 6/2;
-            int b = 1;
-            Console.WriteLine(a > b);
         }
 
         // 1. Write a program in C# to calculate the sum of numbers from 1 to n
@@ -34,7 +32,6 @@ namespace recursion_practice
         // 2. Write a program in C# to find the sum of digits of a number using
         // recursion.
         /*
-
         123 -> 6 
         
         123 % 10 = 3
@@ -42,8 +39,6 @@ namespace recursion_practice
         1   % 10 = 1
         total    = 6
          */
-
-
         static int SumDigits(int n)
         {
             //base case:
@@ -59,6 +54,10 @@ namespace recursion_practice
 
         // 3. Write a program in C to check a number is a prime number or not
         // using recursion.
+        static bool IsPrime(int n)
+        {
+            return IsPrime(n, 2);
+        }
         static bool IsPrime(int n, int i)
         {
             // i is supposed to start as 2.
@@ -75,5 +74,26 @@ namespace recursion_practice
                 return IsPrime(n, i+1);
             }
         }
+
+        // [spiderman buildings thing]
+        static int CalculatePossiblePaths(int b)
+        {
+            //Console.WriteLine("Current Building = " + b);
+
+            if (b >= 2)
+            {
+                return CalculatePossiblePaths(b - 1) + CalculatePossiblePaths(b - 2);
+            }
+            else if (b == 1)
+            {
+                return CalculatePossiblePaths(b - 1);
+            }
+            // There are no buildings left, a path has been completed.
+            else
+            {
+                return 1;
+            }
+        }
+
     }
 }
